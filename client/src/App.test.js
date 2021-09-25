9fc16785b0a8144c0a94b9d41e1ad54ad0678b5a
burntcarrot <aadhav.n1@gmail.com>
2021-09-25 21:08:07 +0530

    fix(docker): move to env vars in PostgreSQL container

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('check basic rendering', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/peavote/i);
   expect(linkElement).toBeInTheDocument();
 });
